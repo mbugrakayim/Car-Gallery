431990988e297366b9a9bfe23d7801ec4e58bc79
mbugrakayim <mbugrakayim@gmail.com>
2022-03-19 14:29:41 +0300

    Eclipse Deneme

--- src/main/java/com/backend/cargallery/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/backend/cargallery/service/impl/EmployeeServiceImpl.java
@@ -11,16 +11,14 @@ import com.backend.cargallery.dao.EmployeeRepository;
 import com.backend.cargallery.exception.NotFoundException;
 import com.backend.cargallery.model.Employee;
 import com.backend.cargallery.service.EmployeeService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
-
 	@Override
-	public Employee saveEmployee(Employee employee , MultipartFile file) throws IOException {
+	public Employee saveEmployee(Employee employee, MultipartFile file) throws IOException {
 		Employee emp = new Employee();
 		emp.setFirstName(employee.getFirstName());
 		emp.setLastName(employee.getLastName());
@@ -31,22 +29,30 @@ public class EmployeeServiceImpl implements EmployeeService {
 		emp.setImages(file.getBytes());
 		return employeeRepository.save(emp);
 	}
+	
+	public Employee createEmployee(Employee employee) {
+		Employee emp = new Employee();
+		emp.setAddress(employee.getAddress());
+		return employeeRepository.save(emp);
+	}
+
 	@Override
 	public List<Employee> findAllEmployee() {
-		
+
 		return employeeRepository.findAll();
 	}
 
-
 	@Override
 	public Employee findByIdEmployee(Long id) {
-		
-		return employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employe Not Found with id: " +id));
+
+		return employeeRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException("Employe Not Found with id: " + id));
 	}
 
 	@Override
-	public Employee updateEmployee(MultipartFile file ,Employee employee, Long id) throws IOException {
-		Employee emp = employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employe Not Found with id: " +id));
+	public Employee updateEmployee(MultipartFile file, Employee employee, Long id) throws IOException {
+		Employee emp = employeeRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException("Employe Not Found with id: " + id));
 		byte[] image = file.getBytes();
 		emp.setFirstName(employee.getFirstName());
 		emp.setLastName(employee.getLastName());
@@ -56,12 +62,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 		emp.setSalary(employee.getSalary());
 		emp.setImages(image);
 		return employeeRepository.save(emp);
-		
+
 	}
 
 	@Override
 	public void deleteEmployee(Long id) {
-		employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employe Not Found with id: " +id));
+		employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employe Not Found with id: " + id));
 		employeeRepository.deleteById(id);
 
 	}
